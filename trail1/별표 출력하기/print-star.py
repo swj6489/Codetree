b55b0bebbef3c3527035c875a79188df7fc39cd2
swj6489 <swj6489@naver.com>
2026-09-03 20:11:40 +0900

    [Passed] 별표 출력하기 (49ms, 17MB)

---
+++ trail1/별표 출력하기/print-star.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+for i in range(N):
+    for j in range(i+1):
+        print('*',end=' ')
+    print()
+
+for i in range(N):
+    for j in range(N-1-i):
+        print('*',end=' ')
+    print()
