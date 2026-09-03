ee6a17a84b8a5eb72568a2b951b82478d28d98bd
swj6489 <swj6489@naver.com>
2026-09-03 21:17:11 +0900

    [Passed] 함수를 이용해 직사각형 만들기 (69ms, 17MB)

---
+++ trail2/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+
+# Please write your code here.
+
+def one():
+    for i in range(n):
+        for j in range(m):
+            print(1,end='')
+        print()
+
+one()
