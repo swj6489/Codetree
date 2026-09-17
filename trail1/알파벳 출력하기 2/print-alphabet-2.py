8032f1124b04b462c1fa66b1f0c3efded22dfc3d
swj6489 <swj6489@naver.com>
2026-09-17 17:47:20 +0900

    [Passed] 알파벳 출력하기 2 (49ms, 16MB)

---
+++ trail1/알파벳 출력하기 2/print-alphabet-2.py
@@ -0,0 +1,15 @@
+n = int(input())
+cnt = 'A'
+
+for i in range(n):
+    print('  ' * i, end='')
+    
+    for j in range(n - i):
+        print(cnt, end=' ')
+        cnt = chr(ord(cnt) + 1)
+
+        if cnt > 'Z':
+            cnt = 'A'
+    print()
+
+
